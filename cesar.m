c26e4c277da4de84e1b2364e751ab4dad3ba8d7e
rafaroch <rafa_roch_9521@hotmail.com>
2015-10-02 16:11:02 -0500

    Update cesar.m

--- cesar.m
+++ cesar.m
@@ -1,38 +1,62 @@
-function cesar
+%Funcion que codifica y decodifica en Cesar
+function cesar()
+clear all
+clc
 
 opc=input('\n de cesar a alfabeto (1)\n de alfabeto a cesar (2) \nIngrese la opcion que desee : ');
-
+%cadena para guardar los  valores del texto codificado
+cadenanueva='';
 if opc==2
 var=input('Ingrese el valor de la variable: ');
+%nos permite usar esta bandera como un contador
 flag=0;
-abecedario = 'abcdefghijklmn�opqrstuvwxyz';
-abecedario2 = strcat(abecedario(var:27),abecedario(1:(var-1)))
-cadena = 'hola';
-cadenanueva='';
+%arreglo que contiene el abecedario
+abecedario = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQESTUVWXYZ';
+%arreglo que nos da el abecedario de forma ciclica
+abecedario2 = strcat(abecedario(var:54),abecedario(1:(var-1)));
+%variable en la que se guarda el contenido del archivo que contiene el
+%texto
+fid=fopen('texto.txt','r');
+%en este array se almacena lo que la variable fid obtuvo
+cadena=fgets(fid);
+%se creaa la variable del archivo nuevo para almacenar la codificaicion
+archivonuevo=fopen('texto_en_cesar.txt','w');
+
 for i=1:length(cadena)
   for j=1:27
       if cadena(i)== abecedario(j)
           cadenanueva=strcat(cadenanueva,abecedario2(j));
           flag=1;
+          
       end%fin de if 
   end %fin del segundo for 
+  
   if flag==0
       
       cadenanueva=strcat(cadenanueva,{' '});
   end
   flag=0;
-end %fin dle primer for 
-cadenanueva
+end 
+%funcion que permite imprimir el contenido en un archivo apartir de la
+%cadena nueva generada
+fprintf(archivonuevo,'%s',cadenanueva);
+%funcion que cierra el archivo que realiza la lectura
+fclose(fid);
+%funcion que cierra el archivo que realiza la escritura
+fclose(archivonuevo);
 
 end 
 
 if opc==1
     
-    var=input('Ingrese el valor de la variable: ');
+  var=input('Ingrese el valor de la variable: ');
 flag=0;
-abecedario = 'abcdefghijklmn�opqrstuvwxyz';
-abecedario2 = strcat(abecedario(var:27),abecedario(1:(var-1)))
-cadena = 'ipmb';
+abecedario = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQESTUVWXYZ';
+abecedario2 = strcat(abecedario(var:54),abecedario(1:(var-1)));
+%%%%% 
+file=fopen('texto_en_cesar.txt','r');
+cadena=fgets(file);
+archivonuevo2=fopen('texto_normal.txt','w');
 cadenanueva='';
 for i=1:length(cadena)
   for j=1:27
@@ -46,12 +70,17 @@ for i=1:length(cadena)
       cadenanueva=strcat(cadenanueva,{' '});
   end
   flag=0;
-end %fin dle primer for 
-cadenanueva
-   
 end 
+%funcion que permite imprimir el contenido en un archivo apartir de la
+%cadena nueva generada
+fprintf(archivonuevo2,'%s',cadenanueva);
+%funcion que cierra el archivo que realiza la lectura
+fclose(file);
+%funcion que cierra el archivo que realiza la escritura
+fclose(archivonuevo2);
+   
 end
 
 
-
+end
 
